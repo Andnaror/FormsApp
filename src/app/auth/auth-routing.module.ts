818d619528ed88cf80a494a4ac6a19dc91a0a3ca
ANDREE MATEO NARVAEZ ORTIZ <andreenarvaezmo@unimagdalena.edu.co>
2023-09-04 17:48:59 -0500

    clase 233

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
+const routes: Routes = [
+  {
+  path:'',
+    children:[
+      {path:'register' ,component:RegisterPageComponent},
+      {path:'**' , redirectTo:'register'},
+    ]
+  },
+  {
+    path:'**',
+    redirectTo:''
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
